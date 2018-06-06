dcb426d32e07d2817d4230f29bd62a60dcf8f65c
Jmo5896 <jmo5896@moore-clan.net>
2018-06-05 19:16:56 -0600

    currently building the file structure

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burger_table (
+    burger_id INT AUTO_INCREMENT NOT NULL,
+    burger_name VARCHAR(30) NOT NULL,
+    devoured BOOLEAN NOT NULL,
+    PRIMARY KEY(burger_id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,2 @@
+INSERT INTO burger_table (burger_name, devoured)
+VALUES ('cheeseburger', false), ('quesadilla burger', false), ('double bacon cheeseburger', false); 
