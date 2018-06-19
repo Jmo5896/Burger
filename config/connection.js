60e05a9caa8ae8a3c22471de3570b7fbd744b6da
Jmo5896 <jmo5896@moore-clan.net>
2018-06-19 15:55:17 -0600

    heroku fix part 2

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,11 @@
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-  host: 'g3v9lgqa8h5nq05o.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+  host: 'q57yawiwmnaw13d2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
   port: 3306,
-  user: 'z2tbhwpy28fn2i7u',
-  password: 'ocg6t9i1urvwpq1s',
-  database: 'otfmo60c6d6d1s08'
+  user: 'x5hywi69elo80e3g',
+  password: 'ktrm2pqpbe000alz',
+  database: 'r4wwra4a2vlfdktk'
 });
  
 connection.connect(function(err) {
