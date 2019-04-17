35c4a970a65a665bbd4e76488e022055950afd42
Jmo5896 <jmo5896@moore-clan.net>
2019-04-16 19:17:44 -0600

    updated some db values

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -33,7 +33,7 @@ router.post('/api/burgers', function(req, res) {
 
 router.put('/api/burgers/:id', function(req, res) {
     
-    var condition = 'burger_id = ' + req.params.id;
+    var condition = 'id = ' + req.params.id;
   
     console.log('condition', condition);
   
--- db/schema.sql
+++ db/schema.sql
@@ -4,7 +4,7 @@ CREATE DATABASE burgers_db;
 USE burgers_db;
 
 CREATE TABLE burger_table (
-    burger_id INT AUTO_INCREMENT NOT NULL,
+    id INT AUTO_INCREMENT NOT NULL,
     burger_name VARCHAR(30) NOT NULL,
     devoured BOOLEAN DEFAULT false,
     PRIMARY KEY(burger_id)
